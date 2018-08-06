c627def2841806cb58978b28011ba1c30a1e5252
Thiago Diniz <thiagodiniz@thiagos-macbook-pro.local>
2018-08-06 14:02:08 -0300

    Initial Commit

---
+++ Spotlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotlight
+//
+//  Created by Thiago Diniz on 06/08/18.
+//  Copyright © 2018 Thiago Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
